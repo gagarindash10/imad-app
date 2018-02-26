712086237993631e0b2d80fb347678aa6333f7cd
gagarindash10 <gagarindash@gmail.com>
2018-02-26 10:07:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var text=document.getElementById('main-text');
+
+element.innerHTML = 'New Value';
+var element= document.getElementById('img');
+img.onclick= function(){
+    img.style.marginLeft = '100px';
+};
