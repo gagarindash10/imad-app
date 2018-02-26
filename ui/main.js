63b79c0386e5965b59835b4991b40afc6da18716
gagarindash10 <gagarindash@gmail.com>
2018-02-26 10:31:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,7 @@ var text=document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
 var element= document.getElementById('madi');
-var marginLeft = 0;
-function moveright(){
-    marginLeft = marginLeft+10;
-    img.style.marginLeft = marginLeft+'px';
-}
+
 img.onclick= function(){
-    var interval = setInterval(moveRight,100);
+    img.style.marginLeft = '100px';
 };
