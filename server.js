47ffc8c21ec636d1110274faec63696700543a57
gagarindash10 <gagarindash@gmail.com>
2018-03-06 14:17:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/test-db', function(req,res){
         }
         else
         {
-            res.send(JSON.stringify(result.rows));
+            res.send(JSON.stringify(result.rows[0].username));
         }
     });
 });
